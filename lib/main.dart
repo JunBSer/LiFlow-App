ca91c6c09b98dd9f5817b41fd1c9d9bc0555c189
JunBSer <lesharad2006@gmail.com>
2026-02-26 21:41:23 +0300

    feat: removed debug line

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
               colorSchemeSeed: Colors.deepPurple,
               brightness: Brightness.light,
             ),
-            
+            debugShowCheckedModeBanner: false,
             darkTheme: ThemeData(
               colorSchemeSeed: Colors.deepPurple,
               brightness: Brightness.dark,
